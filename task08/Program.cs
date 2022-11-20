a7bed4c91a24b3b3b04436cc3165d78fa2069cc8
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-20 12:38:10 +0300

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int r = 0;
+while(count < a)
+{
+    r = count % 2;
+    if(r == 0);
+    {
+        count++;
+    }
+    Console.WriteLine($"{count} ");
+    count++;
+    }
