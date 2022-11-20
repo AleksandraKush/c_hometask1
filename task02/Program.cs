996fedb3a577c41cf52f2cdc4fd787d46805a349
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-20 12:37:07 +0300

    add task02

---
+++ task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввкедите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if(a > b)
+{
+    Console.WriteLine($"Большее число - {max}, меньшее число - {min}");
+}
+else
+{
+    max = b;
+    min = a;
+    Console.WriteLine($"Большее число - {max}, меньшее число - {min}");
+}
