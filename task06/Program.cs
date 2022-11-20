683956f95f7e6570b81a9caccc00cd37310e9546
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-20 12:37:44 +0300

    add task06

---
+++ task06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+b = a % 2;
+if(b == 0)
+{
+    Console.WriteLine($"Число {a} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является четным");
+}
