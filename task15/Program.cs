28519575005117bd1fe6519458e8b084ed89d284
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-21 23:19:00 +0300

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿bool DayCheck(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.WriteLine("Введите цифру от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 8 && number > 0)
+{
+    bool result = DayCheck(number);
+    Console.WriteLine(result ? "да" : "нет");
+}
+else Console.WriteLine("цифра не подходит под заданный диапазон");
