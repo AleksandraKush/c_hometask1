5684529dd52b7435112a20741ba68e61f5da3345
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-21 23:18:25 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int SelectSecondVigit(int num)
+{
+    int a = num % 100;
+    return a / 10;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 1000 && number > 99)
+{
+    int result = SelectSecondVigit(number);
+    Console.WriteLine($"Вторая цифра числа {number} - {result}");
+}
+else Console.WriteLine("это не трехзначное число");
