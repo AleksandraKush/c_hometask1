df1b5e6e310eaa83ddda7ed074e63df0062779fd
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-21 23:18:42 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿int FindThirdVigit(int num, int res)
+{
+    while(num > 99)
+    {
+        res = num % 10;
+        num = num / 10;
+    }
+    return res;
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if(number > 99)
+{
+    int thirdvigit = FindThirdVigit(number, result);
+    Console.WriteLine($"Третья цифра числа {number} - {thirdvigit}");
+}
+else Console.WriteLine("третьей цифры нет");
