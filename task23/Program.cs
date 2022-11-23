e8bb43364abfd1db96435a830bd9f041923c127f
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-23 20:59:05 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        int a = count * count * count;
+        Console.Write($"{count} ");
+        Console.WriteLine(a);
+        count++;
+    }
+    }
+if(n > 0) Cube(n);
+else Console.WriteLine("число не является натуральным");
