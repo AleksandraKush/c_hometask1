a433ca454b17bf7224afdfa35df4b0a4bbacab21
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-23 20:58:45 +0300

    add task21

---
+++ task21/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xc1, int yc1, int zc1, int xc2, int yc2, int zc2)
+{
+    return Math.Sqrt((xc2 - xc1) * (xc2 - xc1) + (yc2 - yc1) * (yc2 - yc1) + (zc2 - zc1) * (zc2 - zc1));
+}
+
+double distance = Distance(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
