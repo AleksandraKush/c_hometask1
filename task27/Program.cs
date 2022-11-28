0e23232180903bb131c5d852c0763caca402f3ae
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-28 23:43:24 +0300

    add task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num)
+{
+    int result = 0;
+    while(num != 0)
+    {
+       result = result + num % 10;
+       num = num / 10;
+    }
+    return result;
+    }
+
+int sum = Sum(number);
+Console.WriteLine(sum);
