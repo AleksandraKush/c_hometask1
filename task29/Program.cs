a0b685b15adedfc181bc757b52fb157ac9cbf86b
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-11-28 23:43:39 +0300

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[8];
+
+void ArrayRandom(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+        if (i == arr.Length - 1) Console.Write($"{arr[i]}]");
+        else Console.Write($"{arr[i]}, ");
+    }
+}
+
+ArrayRandom(array);
