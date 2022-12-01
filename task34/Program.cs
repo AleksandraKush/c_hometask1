7cfe96826ae827913bd97547f7177e0baf9a5e0a
александра <aleksandra@MacBook-Pro-aleksandra.local>
2022-12-01 21:41:24 +0300

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] CreateArrayRnaInt (int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max +1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+      if(i < array.Length - 1) Console.Write($"{array[i]}, ");
+      else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int AmountEven(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) result += 1;
+    }
+    return result;
+}
+int[] arr = CreateArrayRnaInt(5, 100, 1000);
+PrintArray(arr);
+int res = AmountEven(arr);
+Console.WriteLine(res);
